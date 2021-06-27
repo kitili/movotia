07f003cb4acdf8d125024b0d11a8c4c65841b880
your_username <your_email_address@example.com>
2021-06-27 22:30:18 +0300

    js

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+            $("#text-center").submit(function(event) {
+                function flavor() {
+                    (document).ready(function() {
+                            var pizzaNumber = document.getElementById("quantity").value;
+                            return parseInt(pizzaNumber);
+                        }
+
+                        function Order(flavor, size, crust, topping, quantity) {
+                            this.newFlavor = flavor;
+                            this.newSize = size;
+                            this.newCrust = crust;
+                            this.newTopping = topping;
+                            this.newQuantity = quantity;
+                        }
+                        var userInput = new Order(flavor(), size(), crust(), topping(), number());
+                        var totalCost =
+                            (userInput.newSize +
+                                userInput.newCrust +
+                                userInput.newTopping +
+                                userInput.newFlavor) *
+                            userInput.newQuantity; alert("Your charges for Pizza" + totalCost); prompt("enter your email address"); prompt("enter your phone number"); prompt("enter your location"); alert("Your pizza will be delivered"); $("#text-center").reset(); event.preventDefault();
+                    });
+            });
